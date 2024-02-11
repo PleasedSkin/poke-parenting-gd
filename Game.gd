2b23391ab7f92cc8ec434e49914b0bd6656ff634
PleasedSkin <vincent.froger@gmail.com>
2024-02-11 15:47:25 +0100

    Mécanique évolution

--- Game.gd
+++ Game.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var get_pokemon_request := %GetPokemon
 @onready var get_pokemon_species_request := %GetPokemonSpecies
 @onready var get_pokemon_pic_request := %GetPokemonPic
+@onready var get_pokemon_evolution_infos_request := %GetPokemonEvolutionInfos
 @onready var texture_rect = %TextureRect as TextureRect
 @onready var pokemon_name_label = %Name
 @onready var pokemon_level_label = %Niveau
@@ -13,6 +14,10 @@ extends Node2D
 var current_special_menu
 var default_pokemon_sprite
 const default_pokemon_name := "Oeuf"
+var evolution_pokemon_number 
+var can_evolve := false
+var evolution_details
+var evolution_infos_dict = {} # dico clé niveau d'évol et valeur num pokémon d'évol
 
 var level := 0
 
@@ -31,16 +36,21 @@ func _ready() -> void:
 	get_pokemon_request.request_completed.connect(_on_pokemon_request_completed)
 	get_pokemon_species_request.request_completed.connect(_on_pokemon_species_request_completed)
 	get_pokemon_pic_request.request_completed.connect(_on_pokemon_pic_request_completed)
+	get_pokemon_evolution_infos_request.request_completed.connect(_on_pokemon_evolution_infos_request_completed)
 	PokeParentingEvents.points_emitted.connect(_on_poke_parenting_events_points_emitted)
 	PokeParentingEvents.main_item_selected.connect(_on_poke_parenting_events_main_item_selected)
 	#_generate_random_pokemon()
 
 func _generate_random_pokemon() -> void:
 	pokemon_number = rng.randi_range(1, 1025)
-	get_pokemon_request.request(POKE_API_URL + "pokemon/" + str(pokemon_number))
-	get_pokemon_species_request.request(POKE_API_URL + "pokemon-species/" + str(pokemon_number))
+	_generate_targeted_pokemon(pokemon_number)
 
-func _on_pokemon_request_completed(result, response_code, headers, body):
+
+func _generate_targeted_pokemon(pokemon_id: int) -> void:
+	get_pokemon_request.request(POKE_API_URL + "pokemon/" + str(pokemon_id))
+	get_pokemon_species_request.request(POKE_API_URL + "pokemon-species/" + str(pokemon_id))
+
+func _on_pokemon_request_completed(_result, response_code, _headers, body):
 	if response_code == HTTPClient.RESPONSE_OK:
 		var pokemon = JSON.parse_string(body.get_string_from_utf8())
 		# print(pokemon["name"])
@@ -49,7 +59,7 @@ func _on_pokemon_request_completed(result, response_code, headers, body):
 		print_debug("Erreur lors de la récupération du pokémon " + str(pokemon_number))
 
 
-func _on_pokemon_pic_request_completed(result, response_code, headers, body):
+func _on_pokemon_pic_request_completed(_result, response_code, _headers, body):
 	if response_code == HTTPClient.RESPONSE_OK:
 		var image = Image.new()
 		var resultat_chargement = image.load_png_from_buffer(body)
@@ -64,14 +74,33 @@ func _on_pokemon_pic_request_completed(result, response_code, headers, body):
 		print_debug("Erreur lors de la récupération de l'image du pokémon " + str(pokemon_number))
 	
 
-func _on_pokemon_species_request_completed(result, response_code, headers, body):
+func _on_pokemon_species_request_completed(_result, response_code, _headers, body):
 	if response_code == HTTPClient.RESPONSE_OK:
 		var pokemon_species = JSON.parse_string(body.get_string_from_utf8())
-		var corresponding_item = pokemon_species["names"].filter(func(name): return name.language.name == "fr")[0]
+		var corresponding_item = pokemon_species["names"].filter(func(pkmName): return pkmName.language.name == "fr")[0]
 		_change_pokemon_label(corresponding_item.name)
+		var evolution_chain_url = pokemon_species["evolution_chain"]["url"]
+		get_pokemon_evolution_infos_request.request(evolution_chain_url)
 	else:
 		print_debug("Erreur lors de la récupération des données d'espèce pokémon " + str(pokemon_number))
 
+
+func _on_pokemon_evolution_infos_request_completed(_result, response_code, _headers, body):
+	if response_code == HTTPClient.RESPONSE_OK:
+		var evolution_infos = JSON.parse_string(body.get_string_from_utf8())
+		var evolves_to = evolution_infos["chain"]["evolves_to"]
+
+		while evolves_to.size() > 0 && _get_evolution_trigger(evolves_to).size() > 0:
+			var min_level_to_evolve = evolution_details[0].min_level
+			evolution_infos_dict[min_level_to_evolve] = int(evolves_to[0]["species"]["url"].substr((POKE_API_URL + "pokemon-species/").length(), -1).replace("/", ""))
+			evolves_to = evolves_to[0]["evolves_to"]
+	else:
+		print_debug("Erreur lors de la récupération des infos d'évolution du pokémon " + str(pokemon_number))
+
+
+func _get_evolution_trigger(evolves_to):
+	evolution_details = evolves_to[0]["evolution_details"].filter(func(ed): return ed.trigger.name == "level-up")
+	return evolution_details
 
 func _change_pokemon_label(new_label: String):
 	pokemon_name_label.text = "[center]" + new_label + "[/center]"
@@ -87,15 +116,32 @@ func _on_poke_parenting_events_points_emitted(nb_points: int) -> void:
 	pokemon_level_label.text = "[center]Niveau : " + str(level) + "[/center]"
 	if (old_level == 0 && level > 0):
 		_generate_random_pokemon()
+		# pokemon_number = 7
+		# _generate_targeted_pokemon(pokemon_number)
 	elif level == 0:
 		texture_rect.texture = default_pokemon_sprite
 		_change_pokemon_label(default_pokemon_name)
 	else:
-		#if level > old_level
-		pass # vérifier via l'API qu'il existe une évolution pour le pokémon actuel
-		# regarder ensuite les conditions d'évol (niveau, pierre, etc.) et ne faire qu'une évol à la fois
+		var niveaux_evol = evolution_infos_dict.keys()
+		var niveau_pertinent = 0
+		for niv in niveaux_evol:
+			if level >= niv:
+				niveau_pertinent = niv
+		print(niveau_pertinent)
 
-		# if level < old_level : faire éventuellement désévoluer
+		if (niveau_pertinent > 0):
+			evolution_pokemon_number = evolution_infos_dict.get(niveau_pertinent)
+			if evolution_pokemon_number != pokemon_number:
+				pokemon_number = evolution_pokemon_number
+				_generate_targeted_pokemon(evolution_pokemon_number)
+	
+
+	# else:
+	# 	#if level > old_level
+	# 	pass # vérifier via l'API qu'il existe une évolution pour le pokémon actuel
+	# 	# regarder ensuite les conditions d'évol (niveau, pierre, etc.) et ne faire qu'une évol à la fois
+
+	# 	# if level < old_level : faire éventuellement désévoluer
 
 
 func _on_poke_parenting_events_main_item_selected(name: String):
